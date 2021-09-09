2e85582953c2e2e7bfebe647b4a91809fd04ed42
smita Raj <smita.raj12@gmail.com>
2021-09-09 16:33:13 -0700

    fix spacing

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,31 +2,33 @@ using System;
 
 namespace  Bakery.Models
 {
-    public class Bread
-    {
-      public int QuantityOfBread {get; set;}
-      public int TotalCost {get; set;}
+  public class Bread
+  {
+    public int QuantityOfBread {get; set;}
+    public int TotalCost {get; set;}
 
-      public Bread(int quantityOfBread, int totalCost)
-      {
-          QuantityOfBread = quantityOfBread;
-          TotalCost = totalCost;
-      }
-      public int BreadCost(int quantityOfBread)
-      {
-        int OneBreadCost = 5;
-        int freeBread =  quantityOfBread/3;
-        if (quantityOfBread % 3 == 0){
-          TotalCost = (OneBreadCost * quantityOfBread) - (OneBreadCost * freeBread);
-        }
-        else if(quantityOfBread < 3)
-        {
-          TotalCost = OneBreadCost * quantityOfBread;
-        }
-        else{
-          TotalCost =  (OneBreadCost * quantityOfBread) - OneBreadCost;
-        }
-        return TotalCost;
-      }
+    public Bread(int quantityOfBread, int totalCost)
+    {
+      QuantityOfBread = quantityOfBread;
+      TotalCost = totalCost;
     }
+    public int BreadCost(int quantityOfBread)
+    {
+      int OneBreadCost = 5;
+      int freeBread =  quantityOfBread/3;
+      if (quantityOfBread % 3 == 0)
+      {
+        TotalCost = (OneBreadCost * quantityOfBread) - (OneBreadCost * freeBread);
+      }
+      else if(quantityOfBread < 3)
+      {
+        TotalCost = OneBreadCost * quantityOfBread;
+      }
+      else
+      {
+        TotalCost =  (OneBreadCost * quantityOfBread) - OneBreadCost;
+      }
+      return TotalCost;
+    }
+  }
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -5,24 +5,27 @@ namespace  Bakery.Models
   public class Pastry
   {
     public int QuantityOfPastry {get; set;}
-      public int TotalCost {get; set;}
+    public int TotalCost {get; set;}
 
-      public Pastry(int quantityOfPastry, int totalCost)
+    public Pastry(int quantityOfPastry, int totalCost)
+    {
+      QuantityOfPastry = quantityOfPastry;
+      TotalCost = totalCost;
+    }
+    
+    public int PastryCost(int quantityOfPastry)
+    {
+      int OnePastryCost = 2;
+      int freePastry = quantityOfPastry/3;
+      if (quantityOfPastry % 3 == 0)
       {
-          QuantityOfPastry = quantityOfPastry;
-          TotalCost = totalCost;
+        TotalCost = (OnePastryCost * quantityOfPastry) - (freePastry);
       }
-      public int PastryCost(int quantityOfPastry){
-          int OnePastryCost = 2;
-          int freePastry = quantityOfPastry/3;
-          if (quantityOfPastry % 3 == 0){
-          TotalCost = (OnePastryCost * quantityOfPastry) - (freePastry);
-        }
-        else
-        {
-          TotalCost = OnePastryCost * quantityOfPastry;
-        }
-        return TotalCost;
+      else
+      {
+        TotalCost = OnePastryCost * quantityOfPastry;
       }
+      return TotalCost;
+    }
   }
 }      
