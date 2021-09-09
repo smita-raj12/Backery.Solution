cee41e281bf8152c9059425e4e93afacb9970128
smita Raj <smita.raj12@gmail.com>
2021-09-09 14:19:56 -0700

    test pass for calcutating the cost of bread with discount

--- Bakery.Tests/ModelTests/Bread.Test.cs
+++ Bakery.Tests/ModelTests/Bread.Test.cs
@@ -19,5 +19,38 @@ namespace Bakery.Tests
       Assert.AreEqual(quantityOfBread, resultQuantityOfBread);
       Assert.AreEqual(totalCost, ResultTotalCost);
     }
+    [TestMethod]
+    public void BreadDiscount_CalcutateTheCostWithDiscount_MoreThanThree()
+    {
+      int quantityOfBread = 5;
+      int totalCost = 20;
+      Bread bread = new Bread(quantityOfBread, totalCost);
+      int resultQuantityOfBread = bread.QuantityOfBread;
+      int ResultTotalCost = bread.TotalCost;
+      Assert.AreEqual(quantityOfBread, resultQuantityOfBread);
+      Assert.AreEqual(totalCost, 20);
+    }
+    [TestMethod]
+    public void BreadDiscount_CalcutateTheCostWithDiscount_MoreThanfive()
+    {
+      int quantityOfBread = 6;
+      int totalCost = 20;
+      Bread bread = new Bread(quantityOfBread, totalCost);
+      int resultQuantityOfBread = bread.QuantityOfBread;
+      int ResultTotalCost = bread.TotalCost;
+      Assert.AreEqual(quantityOfBread, resultQuantityOfBread);
+      Assert.AreEqual(totalCost, 20);
+    }
+     [TestMethod]
+    public void BreadCost_CalcutateTheCost_WithOneBread()
+    {
+      int quantityOfBread = 1;
+      int totalCost = 5;
+      Bread bread = new Bread(quantityOfBread, totalCost);
+      int resultQuantityOfBread = bread.QuantityOfBread;
+      int ResultTotalCost = bread.TotalCost;
+      Assert.AreEqual(quantityOfBread, resultQuantityOfBread);
+      Assert.AreEqual(totalCost, 5);
+    }
   }
 }  
