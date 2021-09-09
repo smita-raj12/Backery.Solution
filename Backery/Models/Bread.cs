31e7913ddafca1b918dab34d5e8a6dd4a8bf1af1
smita Raj <smita.raj12@gmail.com>
2021-09-09 13:23:34 -0700

    add discount bread functionality

---
+++ Backery/Models/Bread.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace  Backery
+{
+    public class Bread
+    {
+      public int QuantityOfBread {get; set;}
+      public int TotalCost {get; set}
+
+      public Bread(int quantityOfBread, int totalCost)
+      {
+          QuantityOfBread = quantityOfBread;
+          TotalCost = totalCost;
+      }
+      public int BreadCost(int quantityOfBread){
+        int OneBreadCost = 5;
+        int freeBread =  quantityOfBread/3;
+        if (quantityOfBread % 3 == 0){
+          TotalCost = (OneBreadCost * quantityOfBread) - (OneBreadCost * freeBread);
+        }
+        else if(quantityOfBread < 3)
+        {
+          TotalCost = OneBreadCost * quantityOfBread;
+        }
+        else{
+          TotalCost =  (OneBreadCost * quantityOfBread) - OneBreadCost;
+        }
+      }
+    }
+}
