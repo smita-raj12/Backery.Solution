bbc5925503b5f670f864cc2c85a0019ab4df3035
smita Raj <smita.raj12@gmail.com>
2021-09-09 16:05:24 -0700

    test pass for function to calculate the pastries with discount on five pastries

--- Bakery.Tests/ModelTests/Pastry.Test.cs
+++ Bakery.Tests/ModelTests/Pastry.Test.cs
@@ -30,18 +30,18 @@ namespace Bakery.Tests
       Assert.AreEqual(quantityOfPastry, resultQuantityOfPastry);
       Assert.AreEqual(totalCost, 7);
     }
-   /* [TestMethod]
-    public void BreadDiscount_CalcutateTheCostWithDiscount_MoreThanfive()
-    {
-      int quantityOfBread = 6;
-      int totalCost = 20;
-      Bread bread = new Bread(quantityOfBread, totalCost);
-      int resultQuantityOfBread = bread.QuantityOfBread;
-      int ResultTotalCost = bread.TotalCost;
-      Assert.AreEqual(quantityOfBread, resultQuantityOfBread);
-      Assert.AreEqual(totalCost, 20);
-    }
     [TestMethod]
+  public void PastryCost_CalcutateTheCostWithDiscountFor_FivePastries()
+    {
+      int quantityOfPastry = 5;
+      int totalCost = 9;
+      Pastry pastry = new Pastry(quantityOfPastry, totalCost);
+      int resultQuantityOfPastry = pastry.QuantityOfPastry;
+      int ResultTotalCost = pastry.TotalCost;
+      Assert.AreEqual(quantityOfPastry, resultQuantityOfPastry);
+      Assert.AreEqual(totalCost, 9);
+    }
+    /*[TestMethod]
     public void BreadCost_CalcutateTheCost_WithOneBread()
     {
       int quantityOfBread = 1;
